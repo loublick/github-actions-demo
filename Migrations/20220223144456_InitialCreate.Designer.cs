be9dabcace07d25c4ada61825908789f7a2c4f26
loublick <loublick@microsoft.com>
2022-02-23 10:08:15 -0500

    Fixed all outstanding issues.

--- Migrations/20220223144456_InitialCreate.Designer.cs
+++ Migrations/20220223144456_InitialCreate.Designer.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DotNetCoreSqlDb.Migrations
 {
     [DbContext(typeof(MyDatabaseContext))]
-    [Migration("20220223020438_InitialCreate")]
+    [Migration("20220223144456_InitialCreate")]
     partial class InitialCreate
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
